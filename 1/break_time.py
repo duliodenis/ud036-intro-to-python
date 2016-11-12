c955a1d0bc669f2fa9ab3c40a6e3d46b5fa7323d
Dulio Denis <dulio.denis@yahoo.com>
2016-11-12 13:58:21 -0500

    Added a loop

--- 1/break_time.py
+++ 1/break_time.py
@@ -1,5 +1,10 @@
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=e8xni3EcIbc")
+print("Our break started on "+time.ctime())
+for x in range(0,3):
+    time.sleep(10)
+    # webbrowser.open("https://www.youtube.com/watch?v=e8xni3EcIbc")
+    print(x)
+
+print("Our break ended on "+time.ctime())
